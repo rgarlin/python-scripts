1a82320341bb97b600f5938ba10e0b791df499e5
Garlin, Robert <rgarlin@partners.org>
2019-11-22 14:18:44 -0500

    Added port-scan

---
+++ port-scan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.6
+
+## excutes a port scan on a specific subnet  
+
+import nmap
+
+
+nm = nmap.PortScanner() # instantiate nmap.PortScanner object
+f = open('output.txt', 'w')
+
+nm.scan(hosts='10.10.10.0/24', arguments='-n -sP')
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+for host, status in hosts_list:
+    f.write('{0}'.format(host))
+    f.write('\n')
+f.close
+
