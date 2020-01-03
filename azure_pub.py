8e3fd17dc2477b2411463b19fc05264295b170c4
Garlin, Robert <rgarlin@partners.org>
2020-01-03 11:06:39 -0500

    added download file to azure_pub.py

--- azure_pub.py
+++ azure_pub.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3.6
-## Parses the Azure json file and displays the routes received 
+## Downlaod Azure json IP file and 
+## Parses the Azure and displays the routes received for specific service  
 ## by region. 
 
 import json
+import requests
+import wget
+import os
+
+url = 'https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20191216.json'
+
+r = requests.get(url)
+with open('/home/rfg4/temp/temp01/azure-temp.json', 'wb')  as f:
+    f.write(r.content)
+
+
 
 x = input('What region name do you want? ')
 
-with open('azure-public-ip.json', 'r') as json_file:
+
+with open('azure-temp.json', 'r') as json_file:
     data = json.load(json_file)
     for item in data['values']:
         if item['name'] == x:
@@ -14,5 +27,4 @@ with open('azure-public-ip.json', 'r') as json_file:
             print("Change Number: " +  str([item['properties']['changeNumber']]))
             print('Total Routes: ' + str(len(item['properties']['addressPrefixes'])))
             for ip in item['properties']['addressPrefixes']:
-                print(ip)
-          
+                print(ip)          
